e2d2f71652004b653d9f57580be796d662fd0322
Sky Moore <mskymoore@gmail.com>
2020-03-21 02:04:50 -0500

    it works, now just for tweaks and style.

--- corona_plots/views.py
+++ corona_plots/views.py
@@ -1,33 +1,40 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from . import models
+import pickle
+import plotly.offline as po
+import plotly.express as px
 
-lil_key = 'provinces'
-big_key = 'country'
 
 # Create your views here.
 def home(request):
-    context = {
-        'location_keys': {'lil': lil_key, 'big': big_key},
-        'locations': ['locationa', 'locationb', 'locationc'],
-        'location_dict': {
-            'locationa': {
-                lil_key : 'aprovince',
-                big_key : 'acountry'
-            },
-            'locationb': {
-                lil_key : 'bprovince',
-                big_key : 'bcountry'
-            },
-            'locationc': {
-                lil_key : 'cprovince',
-                big_key : 'ccountry'
-            }
-        }
-    }
+    context = {'locations': models.SelectionString.objects.order_by('selection_string')}
     return render(request, 'corona_plots/home.html', context)
 
 
 def plots(request):
+    series_types = ['confirmed', 'death', 'recovered']
+    location = request.GET['location']
+    def generate_graph_div(series_type, selection_string):
+        y_axis = models.SeriesType.objects.filter(
+             series_type=series_type,
+             selection_string=selection_string
+             ).first()
+        y_axis = pickle.loads(models.CountSeries.objects.filter(series_type=y_axis).first().count_series)
+        x_axis = pickle.loads(models.DateSeries.objects.all().first().date_series)[-len(y_axis):]
+        x_axis = [ x_axis[i] for i in range(len(y_axis)) ]
+
+
+        if len(x_axis) == len(y_axis) and len(y_axis) > 0:
+            # fig is plotly figure object and graph_div the html code for displaying the graph
+            fig = px.line(x=x_axis, y=y_axis, title=f'{series_type} cases {selection_string}', labels={'x': 'date', 'y':f'{series_type} cases'})
+            graph_div = po.plot(fig, auto_open = False, output_type="div")
+        
+            return graph_div
+        return ''
+
     context = {
-        'plots': ['plot1.img', 'plot2.img']
+        'graphs': [ generate_graph_div(series_type, location) for series_type in series_types ]
     }
+    
     return render(request, 'corona_plots/plots.html', context)

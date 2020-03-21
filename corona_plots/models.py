c349d4c264d5ff2dad9cb355e19f05b7a577f9fa
Sky Moore <sky.moore@isostech.com>
2020-03-20 19:50:00 -0500

    progress

--- corona_plots/models.py
+++ corona_plots/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class SelectionString(models.Model):
+    selection_string = models.CharField(primary_key=True, max_length=100)
+
+class DateSeries(models.Model):
+    date_retrieved = models.DateTimeField(primary_key=True, default=timezone.now)
+    date_series = models.BinaryField(max_length=None, default=[])
+
+class Location(models.Model):
+    selection_string = models.OneToOneField(SelectionString, on_delete=models.CASCADE)
+    province_state = models.CharField(max_length=100, default='')
+    region_country = models.CharField(max_length=100, default='')
+
+class CountSeries(models.Model):
+    selection_string = models.ForeignKey(SelectionString, on_delete=models.CASCADE)
+    series_type = models.CharField(max_length=100)
+    count_series = models.BinaryField(max_length=None, default=[])
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
--- corona_plots/views.py
+++ corona_plots/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
 
+lil_key = 'provinces'
+big_key = 'country'
+
 # Create your views here.
 def home(request):
     context = {
-        'this_key': 'is a variable accssible in the template'
+        'location_keys': {'lil': lil_key, 'big': big_key},
+        'locations': ['locationa', 'locationb', 'locationc'],
+        'location_dict': {
+            'locationa': {
+                lil_key : 'aprovince',
+                big_key : 'acountry'
+            },
+            'locationb': {
+                lil_key : 'bprovince',
+                big_key : 'bcountry'
+            },
+            'locationc': {
+                lil_key : 'cprovince',
+                big_key : 'ccountry'
+            }
+        }
     }
     return render(request, 'corona_plots/home.html', context)
+
+
+def plots(request):
+    context = {
+        'plots': ['plot1.img', 'plot2.img']
+    }
+    return render(request, 'corona_plots/plots.html', context)

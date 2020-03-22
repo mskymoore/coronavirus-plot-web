0439792a20fe8713f0f1acedf92d4be5017be558
Sky Moore <sky.moore@isostech.com>
2020-03-21 22:14:05 -0500

    working out kinks in models and how data is updated.

--- corona_plots/models.py
+++ corona_plots/models.py
@@ -13,21 +13,21 @@ def create_hash(friendly_name):
     return sha256(friendly_name.encode()).hexdigest()
 
 
-class CaseStatusType(models.Model):
-    id = models.IntegerField(primary_key=True, default=0)
-    name = models.CharField(max_length=50, unique=True)
-    def __str__(self):
-        return str(self.name)
+#class CaseStatusType(models.Model):
+#    id = models.IntegerField(primary_key=True, default=0)
+#    name = models.CharField(max_length=50, unique=True)
+#    def __str__(self):
+#        return str(self.name)
 
 
 class Location(models.Model):
-    id = models.IntegerField(primary_key=True, default=0)
+    #id = models.IntegerField(primary_key=True, default=0)
     province_state = models.CharField(max_length=100, default='')
     region_country = models.CharField(max_length=100, default='')
     latitude = models.CharField(max_length=50)
     longitude = models.CharField(max_length=50)
-    friendly_name = create_friendly_name(province_state, region_country)
-    friendly_hash = models.CharField(default=create_hash(friendly_name), max_length=100)
+    friendly_name = models.CharField(max_length=100)
+    friendly_hash = models.CharField(max_length=100)
     def __str__(self):
         return self.friendly_name
 
@@ -36,6 +36,7 @@ class HistoricEntry(models.Model):
     date = models.DateField()
     count = models.IntegerField(default=0)
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
-    case_status_type_id = models.ForeignKey(CaseStatusType, on_delete=models.DO_NOTHING)
+    case_status_type_id = models.CharField(max_length=25)
+    #case_status_type_id = models.ForeignKey(CaseStatusType, on_delete=models.DO_NOTHING)
     def __str__(self):
        return str(self.date) + ':' + str(self.count)

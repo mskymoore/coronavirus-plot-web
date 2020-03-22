7cbdce74c1009a5c6483850918f7d91320645a6f
Sky Moore <sky.moore@isostech.com>
2020-03-21 22:43:23 -0500

    delete dead code, fix front end to work with new model, update db_test with check before updating.

--- corona_plots/db_test.py
+++ corona_plots/db_test.py
@@ -9,16 +9,7 @@ from hashlib import sha256
 # case_status_type: string, one of ['confirmed', 'deaths', 'recovered']
 def update_database(csv_file, case_status_type_id):
 
-    #case_status_type_name_id_dict = { cst.name: cst for cst in CaseStatusType.objects.all() }
     locs = { loc.friendly_hash: loc for loc in Location.objects.all() }
-
-    #if case_status_type_name not in case_status_type_name_id_dict:
-    #    case_status_type = CaseStatusType(name=case_status_type_name)
-    #    case_status_type.save()
-    #    print('new case status type', str(case_status_type))
-    #    input()
-    #else:
-    #    case_status_type = case_status_type_name_id_dict[case_status_type_name]
 
     for row in csv_file:
 
@@ -48,8 +39,6 @@ def update_database(csv_file, case_status_type_id):
         num_historic_db_entries = len(locations_entries)
         
         list_row = [ item for item in row.items() ][4:]
-        print('\n\n',list_row,'\n\n',locations_entries)
-        input()
 
         for i, entry in enumerate(list_row):
             if int(entry[1]) > 0 :
@@ -58,9 +47,10 @@ def update_database(csv_file, case_status_type_id):
 
         first_new_entry_index = first_nonzero_row_entry_index + num_historic_db_entries
 
-        #if first_new_entry_index > (len(list_row) - 1):
-        #    # no new entries, nothing to do
-        #    break
+        if first_new_entry_index > (len(list_row) - 1):
+            # no new entries, nothing to do
+            print('no updates for', case_status_type_id)
+            break
         
         for entry in list_row[first_new_entry_index:]:
             an_entry = HistoricEntry(
--- corona_plots/views.py
+++ corona_plots/views.py
@@ -1,39 +1,37 @@
 from django.shortcuts import render
+from .coronaVars import case_status_type_names
 from . import models
-import pickle
 import plotly.offline as po
 import plotly.express as px
 
 
 # Create your views here.
 def home(request):
-    context = {'locations': models.SelectionString.objects.order_by('selection_string')}
+    context = {'locations': models.Location.objects.all().order_by('friendly_name')}
     return render(request, 'corona_plots/home.html', context)
 
 
 def plots(request):
-    series_types = ['confirmed', 'death', 'recovered']
     location = request.GET['location']
+    location = models.Location.objects.filter(friendly_name=location).first()
+
     def generate_graph_div(series_type, selection_string):
-        y_axis = models.SeriesType.objects.filter(
-             series_type=series_type,
-             selection_string=selection_string
-             ).first()
-        y_axis = pickle.loads(models.CountSeries.objects.filter(series_type=y_axis).first().count_series)
-        x_axis = pickle.loads(models.DateSeries.objects.all().first().date_series)[-len(y_axis):]
-        x_axis = [ x_axis[i] for i in range(len(y_axis)) ]
+        entries = models.HistoricEntry.objects.filter(location=location,case_status_type_id=series_type).order_by('date')
+        x_axis = []
+        y_axis = []
+        for entry in entries:
+            x_axis.append(str(entry.date))
+            y_axis.append(int(entry.count))
 
 
-        if len(x_axis) == len(y_axis) and len(y_axis) > 0:
-            # fig is plotly figure object and graph_div the html code for displaying the graph
-            fig = px.line(x=x_axis, y=y_axis, title=f'{series_type} cases {selection_string}', labels={'x': 'date', 'y':f'{series_type} cases'})
-            graph_div = po.plot(fig, auto_open = False, output_type="div")
+        fig = px.line(x=x_axis, y=y_axis, title=f'{series_type} cases {selection_string}', labels={'x': 'date', 'y':f'{series_type} cases'})
+        graph_div = po.plot(fig, auto_open = False, output_type="div")
         
-            return graph_div
-        return ''
+        return graph_div
+        
 
     context = {
-        'graphs': [ generate_graph_div(series_type, location) for series_type in series_types ]
+        'graphs': [ generate_graph_div(series_type, location) for series_type in case_status_type_names ]
     }
     
     return render(request, 'corona_plots/plots.html', context)

dd12e198036c8fc8f4490c596d3eae75150e5ae5
Sky Moore <sky.moore@isostech.com>
2020-03-21 21:21:51 -0500

    new data model working.

--- corona_plots/db_test.py
+++ corona_plots/db_test.py
@@ -1,106 +1,73 @@
-import csv, requests, pickle
-from .models import CaseStatusType, Location, HistoricEntry
+import csv, requests
+from .models import CaseStatusType, Location, HistoricEntry, create_friendly_name, create_hash
+from .coronaVars import province_key, country_key, case_files, lat_key, long_key
+from datetime import datetime as dt
 from hashlib import sha256
 
-from github import Github
 
 
-province_key = 'Province/State'
-country_key = 'Country/Region'
+# [(a,2), (b,3), (c,4)] # in db
+# [(z,0), (a,2), (b,3), (c,4), (d,5)] # in row
+#
 
+# csv_file: csv.DictReader
+# case_status_type: string, one of ['confirmed', 'deaths', 'recovered']
+def update_table(csv_file, case_status_type_name):
 
-def get_file(csv_url):
-    return csv.DictReader(requests.get(csv_url).iter_lines(decode_unicode=True))
+    case_status_type_name_id_dict = { cst.name: cst for cst in CaseStatusType.objects.all() }
+    locs = { loc.friendly_hash: loc for loc in Location.objects.all() }
 
+    if case_status_type_name not in case_status_type_name_id_dict:
+        case_status_type = CaseStatusType(name=case_status_type_name)
+        case_status_type.save()
+        #case_status_type_id = case_status_type.id
+    else:
+        case_status_type = case_status_type_name_id_dict[case_status_type_name]
 
-g = Github("34a2755ef5ef33b83bf7bcf4614a27f6287d5589")
+    for row in csv_file:
 
-repo = g.get_repo("CSSEGISandData/COVID-19")
-confirmed = repo.get_contents("csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv")
-death = repo.get_contents("csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv")
-recovered = repo.get_contents("csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv")
-
-list1=[1,2,3]
-list2=[4,5,6]
-for item in zip(list1,list2):
-    print(item)
-exit()
-data_file = get_file(confirmed.download_url)
-print('\n\n',data_file.fieldnames, '\n\n')
-for line in data_file:
-    print(line)
-    input()
-exit()
-
-def generate_count_series(row):
-    return pickle.dumps([int(row[num]) for num in row if (row[num].isnumeric() and int(row[num]) > 0)])
-
-
-def update_table(csv_url_key, series_type):
-
-    update_file = get_file(csv_urls[csv_url_key])
-    locs = { create_hash(loc.friendly_name): loc for loc in Location.objects.all() }
-
-    casetypes = svc.get_casetypes
-    for row in update_file:
         province = row[province_key]
         region = row[country_key]
-        hash_value = create_hash(province + ' - ' + region if province_state is not None  else region
+        lat = row[lat_key]
+        lon = row[long_key]
+
+        hash_value = create_hash(
+                        create_friendly_name(
+                            province,
+                            region
+                        )
+                     )
         
-        #last_entry = HistoricEntry.objects.filter(location_id=location.id).order_by('date').last()
-
-        num_db_entries = len(HistoricEntry.objects.filter(location_id=location.id))
-        cur = locs[hash_value]
-        
-        # [(a,2), (b,3), (c,4)] # in db
-        # [(z,0), (a,2), (b,3), (c,4), (d,5)] # in row
-        #
-
-        for i, item in enumerate(row[]):
-            if item.isnumeric() and int(item) > 0:
-                    first_entry_index = i + 1
-                    break
-        
-
-        start = (e-)
-        for item in row[target:]    
-
-def create_hash(value):
-    return str(sha256(value.encode()))
-
-
-def get_selection_string(row):
-    selection_string = None
-    if row[country_key] != '' and row[province_key].count(',') == 0:
-        if row[province_key] == '':
-            selection_string = SelectionString(selection_string=row[country_key])
+        if hash_value not in locs:
+            location = Location(
+                province_state = province,
+                region_country = region,
+                latitude = lat,
+                longitude = lon
+            )
+            location.save()
         else:
-            selection_string = SelectionString(selection_string=row[province_key] + ' - ' + row[country_key])
-    return selection_string
+            location = locs[hash_value]
+        
+        num_historic_db_entries = len(HistoricEntry.objects.filter(location_id=location))
+        
+        list_row = [ item for item in row.items() ][4:]
+
+        for i, entry in enumerate(list_row):
+            if int(entry[1]) > 0 :
+                first_nonzero_row_entry_index = i
+                break
+
+        first_new_entry_index = first_nonzero_row_entry_index + num_historic_db_entries
+        
+        for entry in list_row[first_new_entry_index:]:
+            HistoricEntry(
+                date = dt.strptime(entry[0], '%m/%d/%y').strftime('%Y-%m-%d'),
+                location_id = location.id,
+                count = int(entry[1]),
+                case_status_type_id = case_status_type
+            ).save()
 
 
-def generate_location(selection_string, row):
-    location = Location(
-                    selection_string=selection_string,
-                    province_state=row[province_key],
-                    region_country=row[country_key]
-                )
-    return location
-
-
-data_file = get_file(confirmed_cases_csv_url)
-
-date_series = DateSeries(date_series=pickle.dumps(data_file.fieldnames[4:])).save()
-
-
-
-for row in data_file:
-    selection_string = get_selection_string(row)
-    if selection_string is not None:
-        selection_string.save()
-        location = generate_location(selection_string, row)
-        location.save()
-
-
-for csv_url_key in csv_urls:
-        update_table(csv_url_key, csv_url_key)
+for case_file_type in case_files:
+        update_table(case_files[case_file_type], case_file_type)
--- corona_plots/models.py
+++ corona_plots/models.py
@@ -5,27 +5,37 @@ import pickle
 
 # Create your models here.
 
+def create_friendly_name(province, region):
+    return str(province) + ' - ' + str(region) if str(province) is not '' else str(region)
+
+
+def create_hash(friendly_name):
+    return sha256(friendly_name.encode()).hexdigest()
+
+
 class CaseStatusType(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.IntegerField(primary_key=True, default=0)
     name = models.CharField(max_length=50, unique=True)
     def __str__(self):
         return str(self.name)
 
+
 class Location(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.IntegerField(primary_key=True, default=0)
     province_state = models.CharField(max_length=100, default='')
     region_country = models.CharField(max_length=100, default='')
     latitude = models.CharField(max_length=50)
     longitude = models.CharField(max_length=50)
-    friendly_name =  str(province_state) + ' - ' + str(region_country) if province_state is not None  else str(region_country)
-    friendly_hash = models.IntegerField(default=str(sha256(friendly_name.encode())))
+    friendly_name = create_friendly_name(province_state, region_country)
+    friendly_hash = models.CharField(default=create_hash(friendly_name), max_length=100)
     def __str__(self):
         return self.friendly_name
-    
+
+
 class HistoricEntry(models.Model):
     date = models.DateField()
     count = models.IntegerField(default=0)
-    location_id = models.ForeignKey(Location, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
     case_status_type_id = models.ForeignKey(CaseStatusType, on_delete=models.DO_NOTHING)
     def __str__(self):
        return str(self.date) + ':' + str(self.count)

09228fb9bbdc6d3aad2ca9158c0fd62f2e7f5436
Sky Moore <sky.moore@isostech.com>
2020-03-26 10:52:47 -0500

    updates to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-sudo su
-apt install letsencrypt
-certbot certonly --standalone -d ncov.1337.rip
-chown -R admin:admin /etc/letsencrypt
-exit
+sudo apt install letsencrypt
+sudo certbot certonly --standalone -d ncov.1337.rip --agree-tos
+sudo chown -R admin:admin /etc/letsencrypt
 docker-compose up

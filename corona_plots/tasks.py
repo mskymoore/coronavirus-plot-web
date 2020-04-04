2e904e34002b0c943182fd9689af5f0edc51ec3c
Sky Moore <sky.moore@isostech.com>
2020-04-03 19:05:30 -0500

    merging changes to master

--- corona_plots/tasks.py
+++ corona_plots/tasks.py
@@ -1,6 +1,7 @@
 import csv, requests
 from .models import Location, HistoricEntry, create_friendly_name, create_hash
-from .coronaVars import province_key, country_key, case_files, lat_key, long_key
+from .coronaVars import province_key, country_key, csv_urls, get_file
+from .coronaVars import case_status_type_names, lat_key, long_key
 from celery import shared_task
 from celery.signals import worker_ready
 from datetime import datetime as dt
@@ -41,37 +42,31 @@ def update_database(csv_file, case_status_type_id):
         
         list_row = [ item for item in row.items() ][4:]
 
-        #for i, entry in enumerate(list_row):
-        #    if int(entry[1]) > 0 :
-        #        first_nonzero_row_entry_index = i
-        #        break
+        if num_historic_db_entries == len(list_row):
+            # no new entries, nothing to do
+            print('no updates for', case_status_type_id)
+            break 
 
-        #first_new_entry_index = first_nonzero_row_entry_index + num_historic_db_entries
-
-        #if first_new_entry_index > (len(list_row) - 1):
-        #    # no new entries, nothing to do
-        #    print('no updates for', case_status_type_id)
-        #    break
-        
         for entry in list_row[num_historic_db_entries:]:
-        
-            an_entry = HistoricEntry(
+            HistoricEntry(
                 date = dt.strptime(entry[0], '%m/%d/%y').strftime('%Y-%m-%d'),
                 location = location,
                 count = int(entry[1]),
                 case_status_type_id = case_status_type_id
             ).save()
             print('new {status_type} historical entry {num} for {date} {location}'.format(
-                    status_type=case_status_type_id,
-                    num=entry[1],
-                    date=entry[0],
-                    location=location.friendly_name))
+                status_type=case_status_type_id,
+                num=entry[1],
+                date=entry[0],
+                location=location.friendly_name))
             
 
 @shared_task
 def do_data_update():
-    for case_file_type in case_files:
-        update_database(case_files[case_file_type], case_file_type)
+    csv_files = [get_file(csv_url) for csv_url in csv_urls]
+
+    for case_status_type_name, csv_file in zip(case_status_type_names, csv_files):
+        update_database(csv_file, case_status_type_name)
 
 @worker_ready.connect
 def update_data(sender=None, conf=None, **kwargs):
